c1781e6741ead1ed26a8bcb6de9f3307a57ea6e9
Boet Waldbjorn <boetw@me.com>
2017-01-24 23:38:04 -0800

    updated formatting

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
+				var viewHeight = $( window ).height()+'px';
+				console.log(viewHeight);
+				$("parallax").height(viewHeight);
+
         $(window).on("scroll", function() {
             var fromTop = $(window).scrollTop();
             if (fromTop > 575){
             $("header").removeClass("hidden");
+            } else if (fromTop < 575) {
+            	$("header").addClass("hidden");
             }
         });
 })
