166c59915afdd0f0a6472014b263cdc64ed02fa9
Boet Waldbjorn <boetw@me.com>
2017-02-07 18:04:29 -0800

    still formatting

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-$(document).ready(function () {
-				var viewHeight = $( window ).height()+'px';
-				console.log(viewHeight);
-				$("parallax").height(viewHeight);
+$(document).ready(function() {
+    var viewHeight = $(window).height() + 'px';
+    console.log(viewHeight);
+    $("#parallax").height(viewHeight);
+    $("#Wrapper").height(viewHeight);
 
-        $(window).on("scroll", function() {
-            var fromTop = $(window).scrollTop();
-            if (fromTop > 575){
+
+    $(window).on("scroll", function() {
+        var fromTop = $(window).scrollTop();
+        if (fromTop > 575) {
             $("header").removeClass("hidden");
-            } else if (fromTop < 575) {
-            	$("header").addClass("hidden");
-            }
-        });
+        } else if (fromTop < 575) {
+            $("header").addClass("hidden");
+        }
+    });
 })
